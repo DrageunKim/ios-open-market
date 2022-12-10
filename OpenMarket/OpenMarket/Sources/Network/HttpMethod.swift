964d43e9f0be8f022780250f27c4576461658a6e
zhilly11 <zhzh1x4@gmail.com>
2022-12-10 17:23:40 +0900

    refactor: HttpMethod 코드 간략화

--- OpenMarket/OpenMarket/Sources/Network/HttpMethod.swift
+++ OpenMarket/OpenMarket/Sources/Network/HttpMethod.swift
@@ -4,22 +4,11 @@
 //  Copyright © yagom. All rights reserved.
 //
 
-enum HttpMethod {
+enum HttpMethod: String {
     case get
     case post
     case patch
     case delete
     
-    var name: String {
-        switch self {
-        case .get:
-            return "GET"
-        case .post:
-            return "POST"
-        case .patch:
-            return "PATCH"
-        case .delete:
-            return "DELETE"
-        }
-    }
+    var name: String { rawValue.uppercased() }
 }
