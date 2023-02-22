6ed19c99f61384b15df3e7c9f67845bf227e7285
DragonYG <kioswe92@gmail.com>
2023-02-22 20:32:30 +0900

    feat: main 삭제

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -10,9 +10,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureView()
     }
+}
 
-
+extension ViewController {
+    private func configureView() {
+        view.backgroundColor = .systemBackground
+    }
 }
 
